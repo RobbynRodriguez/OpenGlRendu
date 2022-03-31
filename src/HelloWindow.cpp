39d966e41d31a0160341f23effa67d018fd67620
robbyn <robbyn.rodriguez@univ-tlse3.fr>
2022-03-31 15:11:00 +0200

    début Light

--- src/HelloWindow.cpp
+++ src/HelloWindow.cpp
@@ -48,13 +48,17 @@ int main()
         return -1;
     }
 
+    std::cout << "salut" << std::endl;
+
     // configure global opengl state
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
     // build and compile our shader zprogram
     // ------------------------------------
-    Shader ourShader("6.2.coordinate_systems.vs", "6.2.coordinate_systems.fs");
+    std::cout << "sa" << std::endl;
+    Shader ourShader("/home/robbyn/CLionProjects/OpenGlRendu/src/6.2.coordinate_system.vs", "/home/robbyn/CLionProjects/OpenGlRendu/src/6.2.coordinate_system.fs");
+    std::cout << "lut" << std::endl;
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
@@ -101,6 +105,22 @@ int main()
             -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
             -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
+
+
+    glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f),
+            glm::vec3( 2.0f,  5.0f, -15.0f),
+            glm::vec3(-1.5f, -2.2f, -2.5f),
+            glm::vec3(-3.8f, -2.0f, -12.3f),
+            glm::vec3( 2.4f, -0.4f, -3.5f),
+            glm::vec3(-1.7f,  3.0f, -7.5f),
+            glm::vec3( 1.3f, -2.0f, -2.5f),
+            glm::vec3( 1.5f,  2.0f, -2.5f),
+            glm::vec3( 1.5f,  0.2f, -1.5f),
+            glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -120,6 +140,7 @@ int main()
 
     // load and create a texture
     // -------------------------
+    std::cout << "salut" << std::endl;
     unsigned int texture1, texture2;
     // texture 1
     // ---------
@@ -135,6 +156,7 @@ int main()
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
     unsigned char *data = stbi_load("/home/robbyn/CLionProjects/OpenGlRendu/recources/textures/container.jpg", &width, &height, &nrChannels, 0);
+    std::cout << "salut" << std::endl;
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -168,12 +190,65 @@ int main()
         std::cout << "Failed to load texture 2" << std::endl;
     }
     stbi_image_free(data);
-
+    std::cout << "salut" << std::endl;
     // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
     // -------------------------------------------------------------------------------------------
     ourShader.use();
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
+
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+//    glm::mat4 view;
+//    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+//                       glm::vec3(0.0f, 0.0f, 0.0f),
+//                       glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+
+// ICI COMMENCE LES LUMIERES
+
+    glm::vec3 coral(1.0f, 0.5f, 0.31f);
+
+
+//    glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
+//    glm::vec3 toyColor(1.0f, 0.5f, 0.31f);
+//    glm::vec3 result = lightColor * toyColor; // = (1.0f, 0.5f, 0.31f);
+//
+//
+//    glm::vec3 lightColor(0.0f, 1.0f, 0.0f);
+//    glm::vec3 toyColor(1.0f, 0.5f, 0.31f);
+//    glm::vec3 result = lightColor * toyColor; // = (0.0f, 0.5f, 0.0f);
+
+    glm::vec3 lightColor(0.33f, 0.42f, 0.18f);
+    glm::vec3 toyColor(1.0f, 0.5f, 0.31f);
+    glm::vec3 result = lightColor * toyColor; // = (0.33f, 0.21f, 0.06f);
+
+//VAO VBO DES LIGHTS
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+// we only need to bind to the VBO, the container's VBO's data already contains the data.
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+// set the vertex attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    Shader lightingShader = Shader("/home/robbyn/CLionProjects/OpenGlRendu/src/lightShader.vs","/home/robbyn/CLionProjects/OpenGlRendu/src/lightShader.fs");
+
+    lightingShader.use();
+    lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+    lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
+
 
 
     // render loop
@@ -199,11 +274,19 @@ int main()
         ourShader.use();
 
         // create transformations
+
+        const float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        glm::mat4 view;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+
         glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        glm::mat4 view          = glm::mat4(1.0f);
         glm::mat4 projection    = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glBindVertexArray(VAO);
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         // retrieve the matrix uniform locations
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
@@ -216,7 +299,17 @@ int main()
 
         // render box
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+//        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
