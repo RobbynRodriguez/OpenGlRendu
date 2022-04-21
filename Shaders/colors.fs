b443f668f12e1603eab5605f895bf982ce8d5931
RobbynRodriguez <robbyn.rodriguez@univ-tlse3.fr>
2022-04-21 14:54:36 +0200

    Correction 21 avril 2022

--- Shaders/colors.fs
+++ Shaders/colors.fs
@@ -33,19 +33,19 @@ void main()
     vec3 result,ambient;
 //     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
     for(int i = 0 ; i< 3 ; i++){
-        ambient += light[i].ambient * texture(material.diffuse, TexCoords).rgb;
+        ambient += light[i].ambient * vec3(0.6f,0.8f,0.2f);
 
     // diffuse
         vec3 norm = normalize(Normal);
         vec3 lightDir = normalize(light[i].position - FragPos);
         float diff = max(dot(norm, lightDir), 0.0);
-        vec3 diffuse = light[i].diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+        vec3 diffuse = light[i].diffuse * diff * vec3(0.6f,0.8f,0.2f);
 
         // specular
         vec3 viewDir = normalize(viewPos - FragPos);
         vec3 reflectDir = reflect(-lightDir, norm);
         float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-        vec3 specular = light[i].specular * spec * texture(material.specular, TexCoords).rgb;
+        vec3 specular = light[i].specular * spec * vec3(0.6f,0.8f,0.2f);
 
         // attenuation
         float distance    = length(light[i].position - FragPos);
